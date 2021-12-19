475f54182ed4d87e224798d833eb31fcd9e9f162
Skyler <sreedgets@outlook.com>
2021-12-19 09:13:22 -0600

    Added card clicked state and card array shuffle.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { nanoid } from 'nanoid';
+import iconData from './iconData';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    const [icons, setIcons] = useState(fetchIcons());
+    const [score, setScore] = useState(0);
+    const [wincon, setWincon] = useState(false);
+
+    function fetchIcons() {
+        return iconData.map(icon => (
+            {
+                url: icon.url,
+                clicked: false,
+                id: nanoid()
+            }
+        ));
+    }
+
+    function handleClick(id) {
+        const thisIcon = icons.find(icon => icon.id === id);
+        thisIcon.clicked ? console.log('game over') : playRound(id);
+    }
+
+    const playRound = (id) => {
+        setIcons(prev => {
+            return prev.map(icon => {
+                return icon.id === id ? {...icon, clicked: !icon.clicked} : {...icon}
+            });
+        });
+
+        setIcons(prev => {
+            return shuffleArr(prev);
+        });
+
+        setScore(prev => prev + 1);
+    }
+
+    /* const shuffleArr = (arr) => {
+        return [...arr].sort(() => Math.random() - 0.5);
+    } */
+
+    const shuffleArr = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    const iconList = icons.map(icon => {
+        return (
+            <img 
+                className="card" 
+                src={icon.url}
+                key={icon.id}
+                onClick={() => handleClick(icon.id)} 
+            />
+        )
+    });
+
+    return(
+        <main>
+            <h1>Final Fantasy IV Memory Game</h1>
+            <p>Select every icon once without picking the same one twice</p>
+            <p>Highest Score: {score}</p>
+            <div className="card-container">
+                {iconList}
+            </div>
+        </main>
+    );
 }
-
-export default App;
---
+++ src/iconData.js
@@ -0,0 +1,40 @@
+const iconData = [
+    {
+        url: './images/Arcanist_Icon_3.png'
+    },
+    {
+        url: './images/Archer_Icon_3.png'
+    },
+    {
+        url: './images/Bard_Icon_3.png'
+    },
+    {
+        url: './images/Conjurer_Icon_3.png'
+    },
+    {
+        url: './images/Dragoon_Icon_3.png'
+    },
+    {
+        url: './images/Gladiator_Icon_3.png'
+    },
+    {
+        url: './images/Lancer_Icon_3.png'
+    },
+    {
+        url: './images/Marauder_Icon_3.png'
+    },
+    {
+        url: './images/Monk_Icon_3.png'
+    },
+    {
+        url: './images/Pugilist_Icon_3.png'
+    },
+    {
+        url: './images/Rogue_Icon_3.png'
+    },
+    {
+        url: './images/Thaumaturge_Icon_3.png'
+    }
+];
+
+export default iconData;
